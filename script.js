a1a87f9afaab440d09b6da1d5847d2900c1acd90
kumanstory <avrysyn9@gmail.com>
2026-05-23 18:31:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ Leon terbiasa mengurus dirinya sendiri — menyiapkan seragam, sarapan, bahkan t
 
 Motor tua peninggalan ayahnya menjadi satu-satunya teman perjalanan setiap pagi. Suaranya kasar, sering mogok di tengah jalan, tapi Leon tetap setia menyalakannya lagi dan lagi. Ada kenangan tersendiri di balik bunyi mesinnya — seolah motor itu menyimpan suara masa lalu yang tak pernah hilang.
 
-Hari ini, langit tampak cerah. Tahun ajaran baru dimulai, dan Leon resmi naik ke kelas tiga SMA. Tahun terakhirnya. Ia tak menyangka, di tahun inilah hidupnya akan berubah — saat ia dipindahkan ke kelas A, dan pertama kali bertemu Fitri.`;
+Hari ini, langit tampak cerah. Tahun ajaran baru dimulai, dan Leon resmi naik ke kelas tiga SMA. Tahun terakhirnya. Ia tak menyangka, di tahun inilah hidupnya akan berubah — saat ia dipindahkan ke kelas A, dan pertama kali bertemu Fitri.
+`;
 
 
 let index = 0;
@@ -89,7 +90,7 @@ function playNarrator(){
 
   suara.lang = "id-ID";
 
-  suara.rate = 0.9;
+  suara.rate = 0⁸.9;
 
   suara.pitch = 1;
 
@@ -101,6 +102,32 @@ function playNarrator(){
 
 function mulaiChapter(){
 
-  document.getElementById("story").innerHTML = "TEST BERHASIL 😁";
+  if(sedangJalan) return;
+
+  sedangJalan = true;
+
+  let music = document.getElementById("bg-music");
+
+  if(music){
+
+    music.volume = 0.03;
+
+    music.play();
+
+  }
+
+  speechSynthesis.cancel();
+
+  document.getElementById("story").innerHTML = "";
+
+  index = 0;
+
+  ketikText();
+
+  setTimeout(() => {
+
+    playNarrator();
+
+  },100);
 
 }
