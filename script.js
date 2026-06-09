d4adbf00bcbcd1639d498c963f8f42faab6f1c3d
kumanstory <avrysyn9@gmail.com>
2026-06-09 10:26:39 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -42,11 +42,93 @@ function mulaiStory(){
 
 }
 
+/* =========================
+   CHAPTER STORY
+========================= */
+
+let text = `aku sayang kamu`;
+
+let index = 0;
+
+let sedangJalan = false;
+
+/* ===== TYPEWRITER ===== */
+
+function ketikText(){
+
+  if(index < text.length){
+
+    document.getElementById("story").innerHTML += text.charAt(index);
+
+    index++;
+
+    setTimeout(ketikText,40);
+
+  } else {
+
+    sedangJalan = false;
+
+  }
+
+}
+
+/* ===== NARRATOR ===== */
+
+function playNarrator(){
+
+  speechSynthesis.cancel();
+
+  let suara = new SpeechSynthesisUtterance(text);
+
+  suara.lang = "id-ID";
+
+  suara.rate = 0.9;
+
+  suara.pitch = 1;
+
+  speechSynthesis.speak(suara);
+
+}
+
+/* ===== PLAY STORY ===== */
+
+function mulaiChapter(){
+
+  if(sedangJalan) return;
+
+  sedangJalan = true;
+
+  let music = document.getElementById("bg-music");
+
+  if(music){
+
+    music.volume = 0.03;
+
+    music.play();
+
+  }
+
+  speechSynthesis.cancel();
+
+  document.getElementById("story").innerHTML = "";
+
+  index = 0;
+
+  ketikText();
+
+  setTimeout(() => {
+
+    playNarrator();
+
+  },100);
+
 function toggleMenu(){
 
-    document
+  document
     .getElementById("sideMenu")
     .classList
     .toggle("active");
+
+}
 
 }
